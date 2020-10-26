40fddf46e9be92fe748bf00145ada8bbbd0ff99b
yashrajchhabra <yashrajchhabra@gmail.com>
2020-10-26 18:16:12 +0530

    code cleanup

--- src/store/index.js
+++ src/store/index.js
@@ -5,22 +5,14 @@ import modules from './modules'
 Vue.use(Vuex);
 
 export const MUTATIONS = {
-    SET_SALES_REP: 'setSalesRep'
 };
 
 export const ACTIONS = {
-    LOAD_CUSTOMER_CONTEXT: 'loadCustomerContext'
 };
 
 export default new Vuex.Store({
-    state: {
-        salesRep: null
-    },
-    mutations: {
-        [MUTATIONS.SET_SALES_REP](state, {salesRep}) {
-            state.salesRep = salesRep
-        }
-    },
+    state: {},
+    mutations: {},
     actions: {},
     modules
 })
