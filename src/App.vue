e7e627049fa68d6852f8b333a79b26f1f63f00ba
yashrajchhabra <yashrajchhabra@gmail.com>
2020-10-26 18:22:22 +0530

    Add nav bar for navigation between menu and stats

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,28 @@
 <template>
     <div id="app">
+        <div class="nav bm-flex ai-center">
+            <router-link to="/menu">Menu</router-link>
+            <router-link to="/stats">Statistics</router-link>
+        </div>
         <router-view/>
     </div>
 </template>
 
 <style lang="scss">
+    .nav {
+        background: $bm-white;
+        height: 70px;
+        color: $bm-red;
+        padding: 0 2rem;
 
+        a {
+            text-decoration: none;
+            margin-right: 2rem;
+            color: $bm-grey;
+
+            &.router-link-active {
+                color: $bm-red;
+            }
+        }
+    }
 </style>
